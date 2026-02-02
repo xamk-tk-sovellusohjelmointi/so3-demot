3a92d7ebda645ebd3891f607e3ed26e44949b764
herixamk <henri.riissanen@xamk.fi>
2026-02-02 20:58:08 +0200

    Demo 03 Finalized

--- demo03/index.ts
+++ demo03/index.ts
@@ -1,109 +1,91 @@
-import express from 'express';
-import path from 'path';
+import express from "express";
+import path from "path";
 import { prisma } from "./lib/prisma";
-import multer, { type FileFilterCallback } from 'multer';
-import fs from 'fs/promises';
+import multer, { type FileFilterCallback } from "multer";
+import fs from "fs/promises";
 
-const app : express.Application = express();
-const port : number = Number(process.env.PORT) || 3003;
+const app: express.Application = express();
+const port: number = Number(process.env.PORT) || 3003;
 
-// Kaikkien latausten käsittelijä middleware
-const uploadHandler : express.RequestHandler = multer({
-    // Väliaikainen sijainti latauksille
+// Määritetään multer ladattujen tiedostojen käsittelyä varten
+const uploadHandler: express.RequestHandler = multer({
+    // Mihin multer tallentaa ladatut kuvat. Voidaan ajatella tässä demossa väliaikaisena sijaintina ennen pysyvää tallennusta
     dest: path.resolve(import.meta.dirname, "tmp"),
-    // Tarkistetaan tiedostomuoto ennen levylle kirjoittamista
-    fileFilter: (req : express.Request, file : Express.Multer.File, callback : FileFilterCallback) => {
-        const sallitutKuvatyypit = ["image/jpeg", "image/jpg"];
+    // Tarkistetaan kuvan koko ennen lataamista palvelimelle
+    limits: {
+        fileSize: 1024 * 1024 * 0.5, // Sallitaan alle 500kt tiedostot
+    },
+    // Tarkistetaan tiedostomuoto
+    fileFilter: (req, file, callback,) => {
+        const sallitutKuvatyypit = ["image/jpeg", "image/gif"];
 
         // Jos tiedostomuoto on sallittu
         if (sallitutKuvatyypit.includes(file.mimetype)) {
             callback(null, true); // Hyväksytään
         } else {
-            callback(new Error("Vain JPG/JPEG kuvat sallittu"));
+            callback(new Error());
         }
     },
-    // Tarkistetaan ennen lataamista palvelimelle
-    limits: {
-        fileSize: (1024 * 1024 * 1.5) // Sallitaan alle 1.5MB tiedostot
-    }
+    
 }).single("file");
-
-
 
 app.set("view engine", "ejs");
 app.use(express.static(path.resolve(import.meta.dirname, "public")));
 
-
 // Otetaan middleware käyttöön yksittäisessä reitissä
-app.post(
-    "/api/upload",
-    uploadHandler,
-    async (req : express.Request, res : express.Response) => {
+app.post("/upload", async (req: express.Request, res: express.Response) => {
 
-        // Jos tiedosto ladataan palvelimelle
-        if (req.file) {
-            // Muodostetaan tiedostolle tiedostonimi. Ladatun tiedoston alkuperäinen nimi ja .jpeg -pääte (riippumatta aiemmasta päätteestä)
-            let tiedostonimi = `${req.file.filename}.jpeg`;
+        // Kutsutaan multer middleware vasta täällä callbackin sisällä virhetilanteiden käsittelyä ja tulostusta varten
+        uploadHandler(req, res, async (err: any) => {
+            // Jos kyseessä on multerin oma validointivirhe, tässä tapauksessa limits-asetuksen aiheuttamana
+            if (err instanceof multer.MulterError) {
+                // Renderöidään lataussivu uudestaan virheilmoituksen kanssa
+                res.render("upload", {
+                    virhe: "Tiedosto on tiedostokooltaan liian suuri (> 500kt).",
+                    teksti: req.body.teksti,
+                });
+            } else if (err) { // Jos kyseessä on tiedostomuotoon liittyvä oma virhe (new Error())
+                res.render("upload", {
+                    virhe: "Väärä tiedostomuoto. Käytä ainoastaan jpg-kuvia",
+                    teksti: req.body.teksti,
+                });
+            } else {
+                // Jos validointi menee läpi ja tiedosto ladataan palvelimelle
+                if (req.file) {
+                    // Määritetään tiedoston tallentamista varten varsinainen tiedostonimi päätteen kanssa.
+                    let tiedostonimi: string = `${req.file.filename}.jpeg`;
 
-            // Kopioidaan kuva väliaikaisten tiedostojen kansiosta staattisiin tiedostoihin img-kansioon
-            await fs.copyFile(
-                path.resolve(import.meta.dirname, "tmp", req.file.filename), // Alkuperäinen tiedosto ilman päätettä
-                path.resolve(import.meta.dirname, "public", "img", tiedostonimi) // Kopio uudessa sijainnissa päätteen kanssa
-            );
+                    // Kopioidaan kuva väliaikaisten tiedostojen kansiosta staattisiin tiedostoihin public/img
+                    await fs.copyFile(
+                        path.resolve(import.meta.dirname, "tmp", req.file.filename), // Alkuperäinen tiedosto tmp-kansiosta ilman päätettä
+                        path.resolve(import.meta.dirname, "public", "img", tiedostonimi), // Kopio uudessa sijainnissa public/img päätteen kanssa
+                    );
 
-            await prisma.kuva.create({
-                data : {
-                    teksti : req.body.description || "Nimetön kuva",
-                    tiedosto : tiedostonimi
+                    // Tallennetaan kuvan muut tiedot tietokantaan.
+                    await prisma.kuva.create({
+                        data: {
+                            teksti: req.body.description || "Nimetön kuva",
+                            tiedosto: tiedostonimi,
+                        },
+                    });
+
+                    // Nyt kuva itsessään on palvelimen staattisissa tiedostoissa ja sen hakemiseen tarvittavat tiedot ovat Prisman tietokannassa
                 }
-            });
-        }
-        res.redirect("/");
-});
-
-app.get("/api/upload", (req : express.Request, res : express.Response) => {
-    res.render("upload", { virhe : "", teksti : ""});
-});
-
-app.get("/", async (req : express.Request, res : express.Response) : Promise<void> => {
-
-    let kuvat = await prisma.kuva.findMany();
-    res.render("index", { kuvat });
-});
-
-// // Middleware reittien virheiden käsittelylle
-// app.use((err : any, req : express.Request, res : express.Response, next : express.NextFunction) => {
-
-//     if (!err) {
-//         return next();
-//     }
-//     // Jos käsitellään tiedostojen latauksen reittiä
-//     if (req.path === "upload") {
-//         // Jos virhe tulee multerista
-//         if (err instanceof multer.MulterError) {
-//             return res.render(
-//                 "lisaa",
-//                 {
-//                     virhe : "Tiedosto on tiedostokooltaan liian suuri (> 1.5Mt).",
-//                     teksti : req.body.description || ""
-//             });
-//         // Muut palvelimen virheet
-//         } else if (err) {
-//             return res.render("upload", {
-//                         virhe : "Väärä tiedostomuoto. Käytä ainoastaan jpg-kuvia",
-//                         teksti : req.body.description
-//                     });
-//         }
-
-//         next(err);
-//     }
-
-//     res.status(500).json({ virhe : "Palvelimella tapahtui odottamaton virhe" });
-// });
-
-app.listen(
-    port,
-    () => {
-        console.log(`Palvelin käynnistettiin osoitteeseen: http://localhost:${port}`);
-    }
+                res.redirect("/");
+            }
+        });
+    },
 );
+
+app.get("/upload", (req: express.Request, res: express.Response) => {
+    res.render("upload", { virhe: "", teksti: "" });
+});
+
+app.get("/", async (req: express.Request, res: express.Response): Promise<void> => {
+        res.render("index", {kuvat: await prisma.kuva.findMany()});
+    },
+);
+
+app.listen(port, () => {
+    console.log(`Palvelin käynnistettiin osoitteeseen: http://localhost:${port}`);
+});
